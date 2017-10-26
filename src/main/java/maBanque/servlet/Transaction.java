c59ba6b4944134d121d2fbd60cbb20ca040e4765
Maxime Pic <maxime.pic@hotmail.fr>
2017-10-26 12:51:03 +0200

    move templates

--- src/main/java/maBanque/servlet/Transaction.java
+++ src/main/java/maBanque/servlet/Transaction.java
@@ -1,29 +1,19 @@
 package maBanque.servlet;
 
-import maBanque.controller.CompteController;
-import maBanque.model.Compte;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-
-@WebServlet("/virements")
-public class Virement extends HttpServlet {
-    CompteController compteCtrl = new CompteController();
-
+@WebServlet("/transactions")
+public class Transaction extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Compte> accountList = compteCtrl.getAccountsByClientId(1);
-
-        request.setAttribute("accountList", accountList);
-        request.getRequestDispatcher("/templates/virement.xhtml").forward(request, response);
+        request.getRequestDispatcher("/templates/transaction.xhtml").forward(request, response);
     }
 }
